94673b2f513ac686bad77ebaf366f3a0ab15436f
Patrick Harrison <jahman@bellatlantic.net>
2019-06-30 16:41:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,3 +28,41 @@ hash[key] = 1
 {total: 5}[age: 1]
 {'hello'=>1}
 end
+  # 1) updating data in a hash update_counting_hash if key is not present, returns a hash with the provided key assigned to the value of 1
+     Failure/Error: expect(update_counting_hash({total: 5},:age)).to eq({total:5, age: 1})
+
+       expected: {:age=>1, :total=>5}
+            got: {"hello"=>1}
+
+       (compared using ==)
+
+       Diff:
+       @@ -1,3 +1,2 @@
+       -:age => 1,
+       -:total => 5,
+       +"hello" => 1,
+
+     # ./spec/intro_to_ruby_hashes_lab_spec.rb:68:in `block (3 levels) in <top (required)>'
+
+  2) updating data in a hash update_counting_hash if key is present, returns a hash where the key's value is incremented by 1
+     Failure/Error: expect(update_counting_hash({count: 5},:count)).to eq({count: 6})
+
+       expected: {:count=>6}
+            got: {"hello"=>1}
+
+       (compared using ==)
+
+       Diff:
+       @@ -1,2 +1,2 @@
+       -:count => 6,
+       +"hello" => 1,
+
+     # ./spec/intro_to_ruby_hashes_lab_spec.rb:72:in `block (3 levels) in <top (required)>'
+
+Finished in 0.03722 seconds (files took 0.37197 seconds to load)
+10 examples, 2 failures
+
+Failed examples:
+
+rspec ./spec/intro_to_ruby_hashes_lab_spec.rb:66 # updating data in a hash update_counting_hash if key is not present, returns a hash with the provided key assigned to the value of 1
+rspec ./spec/intro_to_ruby_hashes_lab_spec.rb:71 # updating data in a hash update_counting_hash if key is present, returns a hash where the key's value is incremented by 1
