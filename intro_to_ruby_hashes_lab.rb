54f55589fac732a593f7a125a8141d5cdb111426
Patrick Harrison <jahman@bellatlantic.net>
2019-06-30 15:35:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,29 +1,27 @@
 def new_hash
-  new_hash= {}
+  {}
 end
 
 def my_hash
-  my_hash = { :dog => "Harry" }
+  { :dog => "Harry" }
 end
 
 def pioneer
-  pioneer ={:name => "Grace Hopper"}
+  {:name => "Grace Hopper"}
 end
 
 def id_generator
-  id_generator = {:id => 15}
+  {:id => 15}
 end
 
 def my_hash_creator(key,value)
-new_hash = {}
-new_hash[key] = value
-new_hash
+[key]  value
 end
 
 def read_from_hash(hash, key)
-  read_from_hash={:name =>"Steve"},[:name => "Steve"]
+  :name =>"Steve",[:name => "Steve"]
 end
 
 def update_counting_hash(hash, key)
-  update_counting_hash = [:apples => 4]+1
+   [:apples => 4]+1
 end
