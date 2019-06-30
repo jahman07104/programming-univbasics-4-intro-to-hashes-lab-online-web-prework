f948aff5200cdfe18de7dbad17d1d41e8274ddb8
Patrick Harrison <jahman@bellatlantic.net>
2019-06-30 15:42:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,9 +18,9 @@ def my_hash_creator(key,value)
   {key => value}
 end
 
-# def read_from_hash(hash, key)
-#   :name =>"Steve",[:name => "Steve"]
-# end
+def read_from_hash(hash, key)
+  :name =>"Steve",[:name => "Steve"]
+end
 
 # def update_counting_hash(hash, key)
 #   [:apples => 4]+1
